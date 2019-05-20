03cbeea49c94526a58f0dd78466ea4d20d681c90
unknown <rfazzio@fau.edu>
2019-05-20 05:38:47 -0400

    Add Trivia Bot

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Under Construction
+Circuit Trivia Bot
+==================================
+## Description
+This application is a bot used to moderate a trivia session. The creator of the trivia can add the bot to a DIRECT conversation and mention the trivia bot to begin the creation of a trivia session. The trivia bot will offer a maximum of 10 questions you may pose the participants of another conversation. Hit the "Start session now" button to begin the session. Participants will be notified then the quiz will shortly being, participants have 10 seconds to answer each question and the first person to answer will be awarded 2 points where subsequent answers will get 1 point. At the end of the trivia session the top 3 winners are posted in the conversation along with their point totals.
+## Prerequisites
+[![NodeJS](https://img.shields.io/badge/Node.js-6.10.2-brightgreen.svg)](https://nodejs.org) <br/>
+* Developer account on circuitsandbox.net. Get it for free at [developer registration](https://circuit.github.io/).
+* OAuth 2.0 `client_id` and optionally `client_secret`. Get if for free at [circuit.github.com/oauth](https://circuit.github.com/oauth).
+
+## Dependencies
+* [circuit-sdk](https://www.npmjs.com/package/circuit-sdk)
+
+## REST Reference
+Other examples of RESTful API's with circuit can be found [here](https://circuitsandbox.net/rest/v2/swagger/ui/index.html).
+
+## Usage
+1. Clone the respository.
+2. Run : `$ npm install`.
+3. Rename `config.json.template` to `config.json` after adding your `client_id`, `client_secret`, `moderatorConversationId`, and the `quizConversationId` the for the trivia session. The `moderatorConversationId` field should refer to the DIRECT conversation Id the host will have with the bot to create the trivia session. The `quizConversationId` should refer to the conversation Id the trivia session will take place in.
+* Note: The bot must be a part of the conversations it is listening to.
+4. To begin creating the trivia game mention the bot by typing `@triviaBotName new session` in the conversation referred to be the `moderatorConversationId` and the bot will post a form for you to fill out. There is an optional field for the title of the quiz, default will be the date. After you fill out the form you can click "Start session now" to begin.
--- app.js
+++ app.js
@@ -1,63 +1,259 @@
 const Circuit = require('circuit-sdk');
 const config = require('./config.json');
-const express = require('express');
 
-const app = express();
 const client = new Circuit.Client(config.credentials);
 
-// Constants
-const MODERATOR_CONVERSATION_ID = config.moderatorConversationId;
-const QUIZ_CONVERSATION_ID = config.quizConversationId;
-const DOMAIN = config.credentials.domain;
-const HOST = config.host;
+const MODERATOR_CONVERSATION_ID = config.moderatorConversationId; // conversation Id of the covnersation with moderator
+const QUIZ_CONVERSATION_ID = config.quizConversationId; // conversation Id of the trivia quiz
+const TIME_DELAY = 10; // Delay to wait between questions
+let FORM_ID; // Id of the current form (Creation Form or Questions for Trivia)
+let CREATOR_ID; // User Id of the moderator
+let quizForm; // Item Object of the current form
+let sessionOnGoing = false; // to determine if a session is going on
+let participantScoresHashMap = {}; // Hash map for participant scores, indexed by userId
+let quizAnswers = {}; // Hash map of quiz answers, indexed by their form id
+let bot; // Bot to manage the trivia session
+
+// Create the blank form for the moderator to fill out
+const createBlankForm = async (item) => {
+    CREATOR_ID = item.creatorId;
+    FORM_ID = `${Date.now()}_${Math.random()}`;
+    let controls = [{
+        type: 'INPUT',
+        name: `title`, // optional
+        text: 'Enter an optional title',
+    }];
+    for (let i = 1; i <= 10; i++) {
+        const question = [{
+            type: 'LABEL',
+            text: `Question ${i}`
+        },{
+            type: 'INPUT',
+            name: `question${i}`,
+            text: 'Enter the question here...',
+        }, {
+            type: 'LABEL',
+            text: `Answer to question ${i}`
+        },{
+            type: 'INPUT',
+            name: `answer${i}`,
+            text: 'Enter the answer here...',
+        }];
+        controls = [...controls, ...question];
+    }
+    const actionButtons = [{
+        type: 'BUTTON', // submit the form
+        options: [{
+            text: 'Start session now',
+            action: 'submit',
+            notification: 'Form submitted successfully'
+        },{
+        text: 'Cancel',
+        action: 'reset',
+        notification: 'Form cancelled successfully'
+        }]
+    }];
+    controls = [...controls, ...actionButtons];
+    const form = {
+        title: 'Trivia Quiz', // optional
+        id: FORM_ID,
+        controls: controls
+    }
+    const content = {
+        content: 'Trivia Quiz',
+        form: form
+    }
+    quizForm = await client.addTextItem(MODERATOR_CONVERSATION_ID, content);
+}
+
+// Creates a form for each question
+const createForm = (question, answer, total) => {
+    const formId = `${Date.now()}_${Math.random()}`;
+    // Save questions an answers in a hash map for later
+    quizAnswers[formId] = {
+        answer: answer.value,
+        question: question.value
+    };
+    const form = {
+        title: `Question ${total}`,
+        id: formId,
+        controls: [{
+            type: 'LABEL',
+            text: question.value
+        }, {
+            type: 'INPUT',
+            name: `answer`,
+            text: 'Enter your answer here',
+        }, {
+            type: 'BUTTON', // submit the form
+            options: [{
+                text: 'Submit',
+                action: 'submit',
+                notification: 'Form submitted successfully'
+            }]
+        }]
+    };
+    return form;
+}
+
+// Chooses the winners and posts results to the conversation
+const chooseWinners = async (itemId) => {
+    await sleep(TIME_DELAY);
+    await client.addTextItem(QUIZ_CONVERSATION_ID , {
+        parentId: itemId,
+        content: 'And the winners are...'
+    });
+    await sleep(TIME_DELAY / 2);
+    const participants = Object.keys(participantScoresHashMap).map(userId => {
+        return { userId: userId, score: participantScoresHashMap[userId].score };
+    });
+    // Sort particpants based on their scores to choose winner
+    participants.sort((a,b) => a.score > b.score ? -1 : 1);
+    const winnersScores = participants.slice(0 , 3);
+    const winnerUserIds = winnersScores.map(w => w.userId);
+    const winners = !!winnerUserIds.length && await client.getUsersById(winnerUserIds);
+    const nonPositiveScores = winnersScores.every(s => s.score < 1);
+    let content = !winners || nonPositiveScores ? 'Sorry there were no winners.' : 'Winners:\n\n';
+    !nonPositiveScores && winnersScores.forEach((w, i) => {
+        const win = winners.find(u => u.userId === w.userId);
+        const winnerText = `${i + 1}. ${win.displayName} - ${w.score} points.\n`;
+        content += winnerText;
+    });
+    await client.addTextItem(QUIZ_CONVERSATION_ID , {
+        parentId: itemId,
+        content: content
+    });
+}
+
+// Created a new session from the form and runs the quiz
+const createNewSession = async (formEvt) => {
+    const form = formEvt.form;
+    const questions = []; // An array of form questions
+    const title = form.data[0];
+    let total = 0;
+    for (let i = 1; i < form.data.length; i += 2) {
+        const question = form.data[i];
+        const answer = form.data[i + 1];
+        if (question.value.length && answer.value.length) {
+            questions.push(createForm(question, answer, ++total));
+        }
+    }
+    if (!questions.length) {
+        await client.addTextItem(MODERATOR_CONVERSATION_ID , {
+            parentId: formEvt.itemId,
+            content: `There aren't enough questions to make a quiz`
+        });
+        return;
+    }
+    sessionOnGoing = true;
+    await client.addTextItem(MODERATOR_CONVERSATION_ID , {
+        parentId: formEvt.itemId,
+        content: 'Form submitted, will start session.'
+    });
+    const initialPost = await client.addTextItem(QUIZ_CONVERSATION_ID , {
+        subject: title.value ? title.value : `Trivia session ${new Date().toLocaleDateString()}`,
+        content: `I will post ${questions.length} questions. You have 10 seconds to answer each question. First person to answer correctly gets extra points. Get ready, first question is coming up now...`
+    });
+    await sleep(TIME_DELAY);
+    for (const question of questions) {
+        FORM_ID = question.id;
+        quizForm = await client.addTextItem(QUIZ_CONVERSATION_ID, {
+            parentId: initialPost.itemId,
+            form: question
+        });
+        await sleep(TIME_DELAY);
+        await updateForm(quizForm);
+    }
+    await chooseWinners(initialPost.itemId);
+    endSession(); // Resets local variables for a new session
+}
+
+// Updated the form after the time is finished
+const updateForm = async (item) => {
+    const text = {
+        type: 'LABEL',
+        text: `The correct answer was: ${quizAnswers[item.text.formMetaData.id].answer}`
+    };
+    item.text.formMetaData.controls = [item.text.formMetaData.controls[0], text];
+    await client.updateTextItem({
+        itemId: item.itemId,
+        form: item.text.formMetaData
+    });
+}
+
+// Submits the answer, awards points if correct (2 if first)
+const submitAnswer = (evt) => {
+    const userId = evt.submitterId;
+    const userForm = evt.form;
+    // If user hasn't submitted  an answer yet store in hash map and set score to 0
+    if (!participantScoresHashMap[userId]) {
+        participantScoresHashMap[userId] = {
+            score: 0
+        };
+    }
+    // User submitted the correct answer
+    if (userForm.data[0].value === quizAnswers[userForm.id].answer) {
+        if (!quizAnswers[userForm.id].answered) {
+            quizAnswers[userForm.id].answered = true;
+            participantScoresHashMap[userId].score += 2;
+        } else {
+            participantScoresHashMap[userId].score++;
+        }
+    }
+}
+
+// Reset variables for the session
+const endSession = () => {
+    FORM_ID = null;
+    CREATOR_ID = null;
+    quizForm = null;
+    sessionOnGoing = false;
+    participantScoresHashMap = {};
+    quizAnswers = {};
+}
 
 const addEventListeners = () => {
-    console.log('Add Listeners');
+    client.addEventListener('itemAdded', evt => {
+        const item = evt && evt.item;
+        // The item was posted in the moderator conversation
+        if (item.convId === MODERATOR_CONVERSATION_ID) {
+            const createNewSession = item.text.mentionedUsers && item.text.mentionedUsers.includes(bot.userId) && item.text.content.includes('new session');
+            // If the item contains a form and not from the bot itself, create a bank form for the moderator
+            if (!item.text.formMetaData && item.creatorId !== bot.userId && createNewSession) {
+                createBlankForm(item);
+            }
+        }
+    });
+
+    client.addEventListener('formSubmission', async evt => {
+        if (quizForm.itemId === evt.itemId && evt.submitterId !== bot.userId) {
+           if (evt.form.id !== FORM_ID) {
+                return;
+           }
+           // Return so the creator cannot take part in the quiz 
+           if (evt.submitterId === CREATOR_ID) {
+               // If the moderator submits the form from the moderator conversation, create a new session
+               if (!sessionOnGoing && evt.itemId === quizForm.itemId && quizForm.convId === MODERATOR_CONVERSATION_ID) {
+                    createNewSession(evt);
+               }
+           } else {
+                submitAnswer(evt);
+           }
+
+        }
+    });
 }
 
-const initWebhooks = async (token) => {
-    // Delete previous webhooks
-    await fetch(`${DOMAIN}/rest/webhooks`, {
-        method: 'DELETE',
-        headers: { 'Authorization': 'Bearer ' + token }
-    });
-
-    // Register new webhook for USER.SUBMIT_FORM_DATA
-    let webhookId = await fetch(`${DOMAIN}/rest/webhooks`, {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + token
-        },
-        body: `url=${encodeURI(`${HOST}/webhook`)}&filter=USER.SUBMIT_FORM_DATA`
-    });
-    console.log(`Webhook ${webhookId} created for USER.SUBMIT_FORM_DATA`);
-
-    // Register new webhook for CONVERSATION.ADD_ITEM
-    webhookId = await fetch(`${DOMAIN}/rest/webhooks`, {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + token
-        },
-        body: `url=${encodeURI(`${HOST}/webhook`)}&filter=CONVERSATION.ADD_ITEM`
-    });
+// Delay process in seconds
+const sleep = (seconds) => {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
-
-// Express Route seciton for webhooks
-app.get('/webhook', (req, res) => {
-    console.log('hi');
-});
-
-app.post('/webhook', (req, res) => {
-    console.log('hi');
-});
 
 // Start the bot as soon as script is run
 (async () => {
     try {
-        const bot = await client.logon();
+        bot = await client.logon();
         addEventListeners();
-        await initWebhooks(client.accessToken);
-        app.listen(7000, () => console.log('App listening on port 7000'));
     } catch (err) {
         console.error(err);
     }
